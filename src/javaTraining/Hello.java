ce718a06694b8b3d54cd0a96bfe9c09528bfc552
Eunha Jeong <eunhaay@gmail.com>
2019-09-05 23:32:14 +0900

    set encoding

--- src/javaTraining/Hello.java
+++ src/javaTraining/Hello.java
@@ -3,6 +3,6 @@ package javaTraining;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		System.out.println("¾È³çÇÏ¼¼¿ä");
+		System.out.println("ì•ˆë…•í•˜ì„¸ìš”");
 	}
 }
