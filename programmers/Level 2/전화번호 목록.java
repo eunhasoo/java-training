0c74470bfbe04c14c22815ebbc9b0bfa28e192ce
Eunha Jeong <eunhaay@gmail.com>
2020-05-07 22:57:44 +0900

    200507

---
+++ programmers/Level 2/전화번호 목록.java
@@ -0,0 +1,15 @@
+class Solution {
+	public boolean solution(String[] phone_book) {
+		for (int i = 0; i < phone_book.length; i++) {
+			for (int j = i + 1; j < phone_book.length; j++) {
+				if (phone_book[i].length() > phone_book[j].length() 
+					&& phone_book[i].startsWith(phone_book[j]))
+					return false;
+				if (phone_book[i].length() < phone_book[j].length() 
+					&& phone_book[j].startsWith(phone_book[i]))
+					return false;
+			}
+		}
+		return true;
+	}
+}
