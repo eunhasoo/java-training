b4130d8f9882e02c2194489e32f91cf7939ffdde
Eunha Jeong <eunhaay@gmail.com>
2020-06-11 19:57:10 +0900

    200611

--- programmers/Level 2/N개의 최소공배수.java
+++ programmers/Level 2/N개의 최소공배수.java
@@ -1,6 +1,6 @@
 class Solution {
 
-	// 최소공약수 구하기
+	// 최대 공약수 구하기
 	int getGcd(int a, int b) {
 		int big = (a > b) ? a : b;
 		int small = (a > b) ? b : a;
@@ -14,22 +14,23 @@ class Solution {
 		return big;
 	}
 	
-	// 최소공배수 구하기
+	// 최소 공배수 구하기
 	int getLcm(int a, int b) {
 		return (a * b) / getGcd(a, b);
 	}
 	
-    public int solution(int[] arr) {
-    	int res = arr[0];
-    	if (arr.length == 1) {
-    		return res;
-    	}
-    	
-    	// [a, b, c]에서 (a와 b의 최소공배수인) k와 c의 최소공배수 == a, b, c의 최소공배수
-    	for (int i = 1; i < arr.length; i++) {
-    		res = getLcm(res, arr[i]);
-    	}
-    	
-    	return res;
-    }
+	public int solution(int[] arr) {
+		int res = arr[0];
+		if (arr.length == 1) {
+			return res;
+		}
+
+		// [a, b, c]에서 (a와 b의 최소공배수인) k와 c의 최소공배수 == a, b, c의 최소공배수
+
+		for (int i = 1; i < arr.length; i++) {
+			res = getLcm(res, arr[i]);
+		}
+
+		return res;
+	}
 }
